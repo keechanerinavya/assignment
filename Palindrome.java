3156373baffcbec41fc3f906bf50f70ea738ca2d
keechanerinavya <navyasreekechaneri@gmail.com>
2016-02-26 17:28:08 +0530

    Added files via upload
    palindrome

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+package com.altimetrik.assignment.stringpalindrome;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int len=args.length;
+		System.out.println("lenght of the string"+len);
+		if(len>0)
+		{
+			for(String str:args)
+				System.out.println(str);
+			System.out.println("the string in uppercase is:"str.toUpperCase());
+		}
+public class palindrome()
+{
+	StringBuffer b=new StringBuffer(.);
+	b.reverse();
+	if(s1.equals(b));
+	{
+		System.out.println("palindrome");
+		else 
+			System.out.println("not palindrome");
+	}
+	
+	}
+	
+}
+	}
+
+}
