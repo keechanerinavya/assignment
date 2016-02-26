5e12d9647521f1e181ba85fbca67cb39c0727669
keechanerinavya <navyasreekechaneri@gmail.com>
2016-02-26 17:28:57 +0530

    Added files via upload
    string concatenation

---
+++ StringConcatenate.java
@@ -0,0 +1,15 @@
+package com.altimetrik.assignment.string;
+
+public class StringConcatenate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String firstName="navya";
+String secondName="keechaneri";
+String fullName=firstName+secondName;
+System.out.println(fullName);
+String full=firstName.concat(secondName);
+System.out.println(full);
+	}
+
+}
