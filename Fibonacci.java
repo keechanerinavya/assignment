47992c48ad2e28e8f9161c70795f3bd094a4cf7f
keechanerinavya <navyasreekechaneri@gmail.com>
2016-02-26 17:22:46 +0530

    Added files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+package com.altimetrik.assignment.stringpalindrome;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int k;
+Scanner sc= new Scanner(System.in);
+System.out.println("enter two numbers:");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.printf("the two numbers are:",a,b);
+
+
+	}
+
+}
