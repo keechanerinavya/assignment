815d82af5ef2b191aa15d261c52c084dbd5d4c64
keechanerinavya <navyasreekechaneri@gmail.com>
2016-02-26 17:28:30 +0530

    Added files via upload
    string array

---
+++ StringArray.java
@@ -0,0 +1,18 @@
+package com.altimetrik.assignment.string;
+
+import java.util.ArrayList;
+
+public class StringArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> List = new ArrayList<String>();
+		List.add("navya");
+		List.add("sree");
+		System.out.println(List);
+		
+	
+
+	}
+
+}
